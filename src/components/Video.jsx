e2a74fc52a5a25a5cb4e1f1b3c124692158c3bdf
kristjanjansen <kristjan.jansen@gmail.com>
2020-10-01 12:59:43 +0200

    hsl stream

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import * as THREE from "three";
+import Hls from "hls.js";
 
 export const Video = (props) => {
-  const url =
-    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+  //const url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+  const url = `https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8`;
 
   const video = document.createElement("video");
   video.loop = true;
@@ -11,11 +12,27 @@ export const Video = (props) => {
   video.playsinline = true;
   video.crossOrigin = "anonymous";
   video.muted = true;
-  video.setAttribute("playsinline", "");
-  video.setAttribute("webkit-playsinline", "");
-  video.style = "display: none";
-  video.src = url;
-  video.play();
+  //video.style = "display: none";
+
+  if (video.canPlayType("application/vnd.apple.mpegurl")) {
+    video.src = url;
+    video.onerror = (e) => {
+      video.src = url;
+    };
+  } else if (Hls.isSupported()) {
+    const hls = new Hls({
+      manifestLoadingRetryDelay: 5000,
+      manifestLoadingMaxRetry: Infinity,
+    });
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+      hls.loadSource(url);
+    });
+    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+      console.log("a");
+      video.play();
+    });
+  }
 
   const width = props.width || 1;
   return (
--- src/settings/index.jsx
+++ src/settings/index.jsx
@@ -12,37 +12,39 @@ export const SettingsProvider = ({ children }) => {
       <SettingsContext.Provider value={value}>
         {children}
       </SettingsContext.Provider>
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          right: 0,
-          width: "200px",
-          color: "white",
-          padding: "16px",
-          background: "rgba(50,50,50,0.5)",
-          display: "grid",
-          gap: "16px",
-          fontFamily: "sans-serif",
-        }}
-      >
-        <div>First: {first}</div>
-        <input
-          type="range"
-          step="0.01"
-          max="1"
-          value={first}
-          onChange={(e) => setFirst(e.target.value)}
-        />
-        <div>Second: {second}</div>
-        <input
-          type="range"
-          step="0.01"
-          max="1"
-          value={second}
-          onChange={(e) => setSecond(e.target.value)}
-        />
-      </div>
+      {false && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            right: 0,
+            width: "200px",
+            color: "white",
+            padding: "16px",
+            background: "rgba(50,50,50,0.5)",
+            display: "grid",
+            gap: "16px",
+            fontFamily: "sans-serif",
+          }}
+        >
+          <div>First: {first}</div>
+          <input
+            type="range"
+            step="0.01"
+            max="1"
+            value={first}
+            onChange={(e) => setFirst(e.target.value)}
+          />
+          <div>Second: {second}</div>
+          <input
+            type="range"
+            step="0.01"
+            max="1"
+            value={second}
+            onChange={(e) => setSecond(e.target.value)}
+          />
+        </div>
+      )}
     </>
   );
 };
